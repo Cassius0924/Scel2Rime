854a169baef12f579ae3e511af23567738edf86d
Cassius0924 <2670226747@qq.com>
2022-12-14 18:10:40 +0800

    second commit

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-cd ~/Documents/MyProgram/Scel2Rime
+cd ~/Documents/MyProgram/Scel2Rime	#更改成代码文件的绝对路径
 python3 Scel2Rime.py
 mv internet_popular.dict.yaml ~/Library/Rime/luna_pinyin.internet.popular.dict.yaml
 /Library/Input\ Methods/Squirrel.app/Contents/MacOS/Squirrel --reload
